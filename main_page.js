c224bce0aad299cef6338f4039a41e0a486c69d6
George Ivanov <georgizeemod@gmail.com>
2023-11-02 16:05:27 +0200

    Georgi added the finalized beta version of the front-end.

--- main_page.js
+++ main_page.js
@@ -6,20 +6,46 @@ client.onConnectionLost = function (responseObject) {
  
 function toggleTap(tapNumber) {
     var message = new Paho.MQTT.Message(tapNumber.toString());
+    var button = index.getElementById("tap")
     message.destinationName = "settaps";
     client.send(message);
+    if (button.innerHTML == "Toggle Tap 1"){
+        button.innerHTML = "Disable Tap 1"
+    } if (button.innerHTML == "Toggle Tap 2"){
+        button.innerHTML = "Disable Tap 2"
+    } if (button.innerHTML == "Toggle Tap 3"){
+        button.innerHTML = "Disable Tap 3"
+    } if (button.innerHTML == "Disable Tap 1"){
+        button.innerHTML = "Toggle Tap 1"
+    } if (button.innerHTML == "Disable Tap 2"){
+        button.innerHTML = "Toggle Tap 2"
+    } if (button.innerHTML == "DIsable Tap 3"){
+        button.innerHTML = "Toggle Tap 3"
+    }
 }
+function toggle220V(tapNumber){
+    var message = new Paho.MQTT.Message(tapNumber.toString());
+    var button = index.getElementById("220V")
+    message.destinationName = "settaps";
+    client.send(message);
+    if (button.innerHTML == "Enable"){
+        button.innerHTML = "Disable"
+    } if (button.innerHTML == "Disable"){
+        button.innerHTML = "Enable"
+    }
+}
+
  
-    client.connect({
-        onSuccess: function () {
-            console.log("Connected to MQTT broker");
-            client.subscribe("sgarden/weather");
-        },
-        onFailure: function (responseObject) {
-            console.log("Failed to connect: " + responseObject.errorMessage);
-        }
-        });
-        client.onMessageArrived = function (message) {
+client.connect({
+    onSuccess: function () {
+        console.log("Connected to MQTT broker");
+        client.subscribe("sgarden/weather");
+    },
+    onFailure: function (responseObject) {
+        console.log("Failed to connect: " + responseObject.errorMessage);
+    }
+    });
+    client.onMessageArrived = function (message) {
     if (message.destinationName === "sgarden/weather") {
         var payloadString = message.payloadString;
         var data = parseTempHumidityString(payloadString);
